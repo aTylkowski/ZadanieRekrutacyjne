a47c394f7e5c617084b5de43440d3cb655ebe41b
Aleksy Tylkowski <atylkowski@etoto.pl>
2023-03-01 20:21:30 +0100

    Initial Commit

---
+++ ZadanieRekrutacyjne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZadanieRekrutacyjne
+//
+//  Created by Aleksy Tylkowski on 01/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
